f581098f0771f0c28c6f90f90b02a7eafa978a0f
Ralph Borcherds <ralph@mrbconsulting.biz>
2018-03-17 18:53:21 -0700

    week 10

--- myapp/app.js
+++ myapp/app.js
@@ -1,235 +1,165 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const util = require('util');
+const url = require('url');
+const { Client } = require('pg');
+var session = require('express-session');
+var morgan = require('morgan');
+var User = require('./modules/user');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var math = require('./routes/math');
-var urlencodedParser;
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Read the DATABASE_URL environment variable and then parse it out. into the various parts.
+var db_url = url.parse(process.env.DATABASE_URL);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
+var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
+var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
+var host = db_url.host.substr(0, db_url.host.indexOf(':'));
+var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
+var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
+
+
+// Connect to the PostgreSQL server
+const client = new Client({
+    host: host,
+    user: user,
+    database: db,
+    password: pass,
+    port: port
+});
+
+client.connect();
+
+// Set static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.post('/math', function(res, req) {
-    var math = {
-        firstNum: res.body.number1,
-        secondNum: res.body.number2,
-        operator: res.body.operator1
+// set our application port
+app.set('port', 5000);
+
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('routes', path.join(__dirname, 'routes'));
+
+
+// Body Parser Middle Ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser. 
+app.use(cookieParser());
+
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+    key: 'user_sid',
+    secret: 'imdkaf8dck)6e#ylzr3vsvi^_6xlmig@d@7pmy5tn@coqt(l&k',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
     }
+}));
 
-    var answer = eval(math.firstNum + math.operator + math.secondNum)
-    req.render('math', {
-        num1: math.firstNum,
-        num2: math.secondNum,
-        operator1: math.operator,
-        answer: answer
-    });
-})
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
 
-app.post('/postage', function(req, res) {
-
-    var typeOfLetter = req.body.mailType;
-    var weightOfLetter = req.body.weight;
-    var priceTotal = calculateRate(typeOfLetter, weightOfLetter);
-    var price;
-    if (priceTotal == 'Package too Large') {
-        price = 'Package Too Large for this type of mailer'
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard');
     } else {
-        price = priceTotal;
+        next();
     }
+};
 
-    res.render('postage', {
-        title: 'Postage Calculator',
-        typeOfLetter: typeOfLetter,
-        weight: weightOfLetter,
-        price: price
+// route for Home-Page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+
+// route for user signup
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html');
     })
+    .post((req, res) => {
+        User.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+            })
+            .then(user => {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            })
+            .catch(error => {
+                res.redirect('/signup');
+            });
+    });
 
-})
 
-// catch 404 and forward to error handler
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function(user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/dashboard.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for handling 404 requests(unavailable routes)
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    res.status(404).send("Sorry can't find that!")
 });
 
 
-function calculateRate(typeOfLetter, weightOfLetter) {
-
-    if ((typeOfLetter == 'Letters (Stamped)' || typeOfLetter == 'Letters (Metered)') && weightOfLetter > 3.5) {
-        typeOfLetter = 'Large Envelopes (Flats)';
-    }
-    var price;
-    switch (typeOfLetter) {
-        case 'Letters (Stamped)':
-            switch (true) {
-                case (weightOfLetter <= 1):
-                    price = 0.50;
-                    break;
-
-                case (weightOfLetter > 1 && weightOfLetter <= 2):
-                    price = 0.71;
-                    break;
-
-                case (weightOfLetter > 3 && weightOfLetter <= 3):
-                    price = 0.92;
-                    break;
-
-                case (weightOfLetter > 3 && weightOfLetter <= 3.5):
-                    price = 1.13;
-                    break;
-            }
-            break;
-
-        case 'Letters (Metered)':
-            switch (true) {
-                case (weightOfLetter <= 1):
-                    price = 0.47;
-                    break;
-
-                case (weightOfLetter > 1 && weightOfLetter <= 2):
-                    price = 0.68;
-                    break;
-
-                case (weightOfLetter > 2 && weightOfLetter <= 3):
-                    price = 0.89;
-                    break;
-
-                case (weightOfLetter > 3 && weightOfLetter <= 3.5):
-                    price = 1.10;
-                    break;
-            }
-            break;
-
-        case 'Large Envelopes (Flats)':
-            switch (true) {
-                case (weightOfLetter <= 1.0):
-                    price = 1;
-                    break;
-
-                case (weightOfLetter > 1 && weightOfLetter <= 2.0):
-                    price = 1.21;
-                    break;
-
-                case (weightOfLetter > 2 && weightOfLetter <= 3.0):
-                    price = 1.42;
-                    break;
-
-                case (weightOfLetter > 3 && weightOfLetter <= 4.0):
-                    price = 1.63;
-                    break;
-
-                case (weightOfLetter > 4 && weightOfLetter <= 5.0):
-                    price = 1.84;
-                    break;
-
-                case (weightOfLetter > 5 && weightOfLetter <= 6.0):
-                    price = 2.05;
-                    break;
-
-                case (weightOfLetter > 6 && weightOfLetter <= 7.0):
-                    price = 2.26;
-                    break;
-
-                case (weightOfLetter > 7 && weightOfLetter <= 8.0):
-                    price = 2.47;
-                    break;
-
-                case (weightOfLetter > 8 && weightOfLetter <= 9.0):
-                    price = 2.68;
-                    break;
-
-                case (weightOfLetter > 9 && weightOfLetter <= 10.0):
-                    price = 2.89;
-                    break;
-
-                case (weightOfLetter > 10 && weightOfLetter <= 11.0):
-                    price = 3.10;
-                    break;
-
-                case (weightOfLetter > 11 && weightOfLetter <= 12.0):
-                    price = 3.31;
-                    break;
-
-                case (weightOfLetter > 12 && weightOfLetter <= 13.0):
-                    price = 3.52;
-                    break;
-
-                case (weightOfLetter > 13):
-                    price = 'Package too Large';
-                    break;
-            }
-            break;
-
-        case ('First-Class Package Service-Retail'):
-            switch (true) {
-                case (weightOfLetter <= 4):
-                    price = 3.5;
-                    break;
-
-                case (weightOfLetter > 4 && weightOfLetter <= 8):
-                    price = 3.75;
-                    break;
-
-                case (weightOfLetter > 8 && weightOfLetter <= 9):
-                    price = 4.1;
-                    break;
-
-                case (weightOfLetter > 9 && weightOfLetter <= 10):
-                    price = 4.45;
-                    break;
-
-                case (weightOfLetter > 10 && weightOfLetter <= 11):
-                    price = 4.8;
-                    break;
-
-                case (weightOfLetter > 11 && weightOfLetter <= 12):
-                    price = 5.15;
-                    break;
-
-                case (weightOfLetter > 12 && weightOfLetter <= 13):
-                    price = 5.5;
-                    break;
-
-                case (weightOfLetter > 13):
-                    price = 'Package too Large';
-                    break;
-            }
-            break;
-
-        default:
-            price: 'unknown package';
-            break;
-    }
-
-    return price;
-}
-
-module.exports = app;
+// start the express server
+app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
--- myapp/routes/math.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.post('/', function(req, res, next) {
-    res.render('math', { title: 'Math Results' });
-});
-
-module.exports = router;
