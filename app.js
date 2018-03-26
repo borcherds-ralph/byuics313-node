af5d88341641bdbaa15df3b35cbe6064e427a173
Ralph Borcherds <ralph@mrbconsulting.biz>
2018-03-26 13:18:24 -0700

    heroku fixes

--- app.js
+++ app.js
@@ -193,3 +193,5 @@ app.use(function(req, res, next) {
 
 // start the express server
 app.listen(app.get('nodeport'), () => console.log(`App started on port ${app.get('nodeport')}`));
+
+module.exports = app;
